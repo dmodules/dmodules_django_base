90834f080176e20df84775cc92d8ead6059cdb3f
Martin C <info@d-modules.com>
2016-12-06 14:09:27 -0500

    base files

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM dmodules/dmodules-django-docker:latest
+
+ADD requirements.txt /localapp/
+RUN pip install -r requirements.txt
+
+COPY . /localapp
+
+# collectstatic
+# -------------
+RUN DJANGO_MODE=build python manage.py collectstatic --noinput
+RUN DJANGO_MODE=build python manage.py migrate
---
+++ settings.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+import os
+gettext = lambda s: s
+DATA_DIR = os.path.dirname(os.path.dirname(__file__))
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost', '*',]
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '@lqhi6_f-2nqgn&tck3t_b=+darg9+3172@@x+*3tt%kncrgjt'
+
+TEMPLATE_DEBUG = True
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pylint',
+)
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    #default dmodules apps
+    #'django_jenkins'
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'urls'
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'America/Montreal'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATICFILES_DIRS = (('/localapp/static'),)
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
+STATIC_ROOT = os.path.join(DATA_DIR, 'static_collected')
